c08bb5e6aca21514f07a3f22cbbcb492856c2938
Jared Fraser <j.fraser@modsognir.com>
2011-08-17 15:16:03 +1000

    Interpolate gsub results

--- lib/princely/pdf_helper.rb
+++ lib/princely/pdf_helper.rb
@@ -31,10 +31,10 @@ module PdfHelper
     
     # Make all paths relative, on disk paths...
     html_string.gsub!(".com:/",".com/") # strip out bad attachment_fu URLs
-    html_string.gsub!( /src=["']+([^:]+?)["']/i ) { |m| "src=\"#{Rails.public_path}/" + $1 + '"' } # re-route absolute paths
+    html_string.gsub!( /src=["']+([^:]+?)["']/i ) { |m| "src=\"#{Rails.public_path}/#{$1}\"" } # re-route absolute paths
     
     # Remove asset ids on images with a regex
-    html_string.gsub!( /src=["'](\S+\?\d*)["']/i ) { |m| 'src="' + $1.split('?').first + '"' }
+    html_string.gsub!( /src=["'](\S+\?\d*)["']/i ) { |m| "src=\"#{$1.split('?').first}\"" }
     
     # Send the generated PDF file from our html string.
     if filename = options[:filename] || options[:file]
